641feeabaefb8c21035614743912fb02a807df74
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2023-03-26 12:17:27 +0200

    initial commit I started this proj. Vue is kinda weird but eh, we'll work it out.

---
+++ src/components/Button.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps({
+    onClick: {
+        required: false,
+        type: Function,
+    },
+    color: {
+        required: false,
+        type: String,
+        default: 'primary',
+    },
+    variant: {
+        required: false,
+        type: String,
+        default: 'contained',
+    },
+})
+
+const variant = ref('contained-')
+const color = ref('primary')
+
+if (props.variant === 'outlined') {
+    variant.value = 'outlined-'
+}
+
+if (props.color === 'secondary') {
+    color.value = 'secondary'
+}
+</script>
+<template>
+    <button v-on:click="props.onClick? props.onClick($event) : undefined " v-bind:class="variant+color">
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+
+button {
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14);
+    min-width: 64px;
+    padding: 6px 16px;
+    border-radius: 4px;
+    text-transform: uppercase;
+    transition: all 100ms ease-in-out;
+}
+button:hover {
+    opacity: 0.8;
+    cursor: pointer;
+}
+button:active {
+    opacity: 0.6;
+}
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import Button from './Button.vue';
+import logo from '../assets/logo.png';
+function log() {
+    console.log('nihha')
+}
+</script>
+
+<template>
+    <nav>
+        <div class="container">
+            <ul>
+                <li><a href="/"><img :src="logo" alt="DTT-Logo"></a></li>
+                <li><a href="houses">Houses</a></li>
+                <li><a href="about">About</a></li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<style>
+nav {
+    background-color: var(--bg-2);
+    width: 100vw;
+    height: 64px;
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14);
+}
+
+ul {
+    padding-left: 0;
+    display: flex;
+    align-items: center;
+    margin: 0;
+    height: 100%;
+}
+li, a > img {
+    padding-right: 1rem;
+    padding-left: 1rem;
+    display: flex;
+    align-items: center;
+    list-style: none;
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: var(--text-1);
+}
+
+a {
+    text-decoration: none;
+    color: var(--text-1);
+}
+
+img {
+    height: 100%;
+    width: 100px;
+    margin-left: 20px;
+}</style>
